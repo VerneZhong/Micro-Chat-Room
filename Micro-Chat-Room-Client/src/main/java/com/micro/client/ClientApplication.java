7e785cc3685c846b96563eda4137f2d0c97b9aa8
Mr.zxb <zhongxuebin171980062@gmail.com>
2020-05-16 20:31:46 +0800

    完善cloud模块的依赖

---
+++ Micro-Chat-Room-Client/src/main/java/com/micro/client/ClientApplication.java
@@ -0,0 +1,17 @@
+package com.micro.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 客户端引导类
+ * @author Mr.zxb
+ * @date 2020-05-16 19:33:11
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
