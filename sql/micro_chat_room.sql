6be78287e9a979a4cb54d172d81b879fa2300aaa
Mr.zxb <zhongxuebin171980062@gmail.com>
2020-05-19 22:47:41 +0800

    新增：用户好友表结构脚本

--- sql/micro_chat_room.sql
+++ sql/micro_chat_room.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : mysql
  Source Server Type    : MySQL
- Source Server Version : 80017
+ Source Server Version : 50535
  Source Host           : localhost:3306
  Source Schema         : micro_chat_room
 
  Target Server Type    : MySQL
- Target Server Version : 80017
+ Target Server Version : 50535
  File Encoding         : 65001
 
- Date: 19/05/2020 16:32:42
+ Date: 19/05/2020 21:35:53
 */
 
 SET NAMES utf8mb4;
@@ -21,16 +21,30 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE `user`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `nickname` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT '昵称',
-  `account` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT '帐号名称',
-  `cellphone_number` char(11) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '手机号码',
-  `is_online` int(2) NOT NULL COMMENT '是否在线',
+  `account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '帐号名称',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户密码',
+  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '昵称',
+  `cellphone_number` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `avatar_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '头像',
+  `age` int(3) NULL DEFAULT NULL COMMENT '年龄',
+  `area` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '地区',
+  `is_locked` int(2) NOT NULL DEFAULT 0 COMMENT '0未锁定，1锁定',
   `register_date` date NOT NULL COMMENT '注册日期',
-  `avatar` varchar(128) COLLATE utf8mb4_general_ci NOT NULL COMMENT '头像',
-  `avatar_thumbnail` varchar(128) COLLATE utf8mb4_general_ci NOT NULL COMMENT '头像缩略图',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `nickname_index`(`nickname`(191)) USING BTREE COMMENT '账号唯一索引'
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for user_friends
+-- ----------------------------
+DROP TABLE IF EXISTS `user_friends`;
+CREATE TABLE `user_friends`  (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账号',
+  `friends_account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '好友账号',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
