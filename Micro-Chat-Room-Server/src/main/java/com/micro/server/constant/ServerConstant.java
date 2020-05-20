ad5c089ca94fceb8adb92a33163e3127b06d88fb
Mr.zxb <zxb@sengled.com>
2020-05-20 16:43:54 +0800

    新增：Netty Server (一)

---
+++ Micro-Chat-Room-Server/src/main/java/com/micro/server/constant/ServerConstant.java
@@ -0,0 +1,16 @@
+package com.micro.server.constant;
+
+/**
+ * 静态常量
+ *
+ * @author Mr.zxb
+ * @date 2020-05-20
+ **/
+public class ServerConstant {
+
+    /**
+     * Netty Server Port
+     *
+     */
+    public static final int SERVER_PORT = 8888;
+}
